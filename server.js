4d72b3266e2d32930cc147d5f2bc4a72dd07dbaa
roshan <roshan@gmail.com>
2023-08-13 23:03:34 +0530

    /roshan

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/home", (req,res) => {
+app.use("/roshan", (req,res) => {
     res.json({
         msg:"Hi I am Roshan"
     })
